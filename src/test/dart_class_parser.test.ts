9d4a26603e250d3a89ec3512cad4502da793f699
Paweł Jakubowski <jkb.pawel@gmail.com>
2023-08-08 21:45:09 +0200

    Add unit tests for parseLinesToClassName function

--- src/test/dart_class_parser.test.ts
+++ src/test/dart_class_parser.test.ts
@@ -1,4 +1,4 @@
-import { doesLookingFurtherMakeSense } from "../dart_class_parser";
+import { doesLookingFurtherMakeSense, parseLinesToClassName } from "../dart_class_parser";
 
 describe("doesLookingFurtherMakeSense", () => {
   test("when passed a line which is build() method signature, returns false ", () => {
@@ -15,5 +15,40 @@ describe("doesLookingFurtherMakeSense", () => {
 
   test("when passed a line which is a closing parenthesis, returns true ", () => {
     expect(doesLookingFurtherMakeSense("),")).toBe(true);
+  });
+});
+
+describe("parseLinesToClassName", () => {
+  test("when passed empty array, returns empty string", () => {
+    expect(parseLinesToClassName([])).toBe("");
+  });
+
+  test("when passed lines which don't contain class name declaration, returns empty string", () => {
+    const lines = [
+      "const Person({",
+      "  required this.name,",
+      "  required this.person,",
+      "});",
+      "",
+      "final String name;",
+      "final int age;",
+    ];
+
+    expect(parseLinesToClassName(lines)).toBe("");
+  });
+
+  test("when passed lines which contain class name declaration in one line, returns properly class name", () => {
+    const lines = ["class MyWidget extends StatefulWidget {"];
+
+    expect(parseLinesToClassName(lines)).toBe("MyWidget");
+  });
+
+  test("when passed lines which contain class name declaration in multiple lines, along with mixin, returns properly class name", () => {
+    const lines = [
+      "class ClassWithVeryLongNameThatWouldNotFitInOneLineBecauseItsTooLong",
+      "   extends StatefulWidget with GreatWidgetMixin {",
+    ];
+
+    expect(parseLinesToClassName(lines)).toBe("ClassWithVeryLongNameThatWouldNotFitInOneLineBecauseItsTooLong");
   });
 });
