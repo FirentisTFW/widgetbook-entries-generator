6bd9a094da26c1c5002c864ee3a82a559377b382
Paweł Jakubowski <jkb.pawel@gmail.com>
2023-08-08 21:44:39 +0200

    Format test code using prettier

--- src/test/dart_class_parser.test.ts
+++ src/test/dart_class_parser.test.ts
@@ -1,19 +1,19 @@
-import { doesLookingFurtherMakeSense } from '../dart_class_parser';
+import { doesLookingFurtherMakeSense } from "../dart_class_parser";
 
-describe('doesLookingFurtherMakeSense', () => {
-    test('when passed a line which is build() method signature, returns false ', () => {
-        expect(doesLookingFurtherMakeSense('Widget build(BuildContext context)')).toBe(false);
-    });
+describe("doesLookingFurtherMakeSense", () => {
+  test("when passed a line which is build() method signature, returns false ", () => {
+    expect(doesLookingFurtherMakeSense("Widget build(BuildContext context)")).toBe(false);
+  });
 
-    test('when passed a line which is createState() method signature, returns false ', () => {
-        expect(doesLookingFurtherMakeSense('createState() =>')).toBe(false);
-    });
+  test("when passed a line which is createState() method signature, returns false ", () => {
+    expect(doesLookingFurtherMakeSense("createState() =>")).toBe(false);
+  });
 
-    test('when passed a line which is a variable declaration, returns true ', () => {
-        expect(doesLookingFurtherMakeSense('final String name')).toBe(true);
-    });
+  test("when passed a line which is a variable declaration, returns true ", () => {
+    expect(doesLookingFurtherMakeSense("final String name")).toBe(true);
+  });
 
-    test('when passed a line which is a closing parenthesis, returns true ', () => {
-        expect(doesLookingFurtherMakeSense('),')).toBe(true);
-    });
+  test("when passed a line which is a closing parenthesis, returns true ", () => {
+    expect(doesLookingFurtherMakeSense("),")).toBe(true);
+  });
 });
