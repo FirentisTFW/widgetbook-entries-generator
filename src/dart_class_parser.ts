86f91370917367a9bbad66b24609f85d303644c0
FirentisTFW <jkb.pawel@gmail.com>
2023-08-15 22:28:55 +0200

    Add ESLint

--- src/dart_class_parser.ts
+++ src/dart_class_parser.ts
@@ -216,8 +216,8 @@ class DartClassConstructor {
 }
 
 class DartClassField {
-  name: string = "";
-  type: string = "";
+  name: string;
+  type: string;
 
   constructor(name: string, type: string) {
     this.name = name;
