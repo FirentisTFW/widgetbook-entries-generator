bb525f8682d3d426ae0f8c026d9312b085140a5e
Paweł Jakubowski <jkb.pawel@gmail.com>
2023-08-23 21:55:20 +0200

    Add a comment explaining the need of a factory for generating paths

--- src/generators/path/factory.ts
+++ src/generators/path/factory.ts
@@ -2,8 +2,12 @@ import { PathGenerator, PathGeneratorImpl } from "./generator";
 
 abstract class PathGeneratorFactory {
   static create(): PathGenerator {
-    // FIXME Write a comment explaining we can have more implementations in the future,
-    //  for that we use the factory.
+    // For now we only have one implementation of PathGenerator, but in the future
+    // various strategies of generating widgetbook entries might be available.
+    // That's why we use a factory here.
+    //
+    // In this function, based on some configuration settings,
+    // a particular strategy might be chosen.
     return new PathGeneratorImpl();
   }
 }
