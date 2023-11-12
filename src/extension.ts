f0306df7ba69ade440971014ea909ede15926e03
Paweł Jakubowski <jkb.pawel@gmail.com>
2023-11-12 11:59:48 +0100

    Implement generating widgetbook entries for the whole directory (to be refactored)

--- src/extension.ts
+++ src/extension.ts
@@ -2,17 +2,20 @@ import * as vscode from "vscode";
 import { WidgetbookEntriesCodeActions } from "./code_actions/widgetbook_entries";
 import { parseTextToClass } from "./util/dart_class_parser";
 import { writeWidgetbookEntry } from "./util/file_util";
+import path = require("path");
 
 const DART_MODE = { language: "dart", scheme: "file" };
 
 function activate(context: vscode.ExtensionContext) {
-  const command = vscode.commands.registerCommand(
-    "widgetbook-generator.generate.widget",
-    generateWidgetbookEntryForWidgetInScope
-  );
-
   context.subscriptions.push(
-    command,
+    vscode.commands.registerCommand(
+      "widgetbook-generator.generate.directory",
+      generateWidgetbookEntriesForDirectory
+    ),
+    vscode.commands.registerCommand(
+      "widgetbook-generator.generate.widget",
+      generateWidgetbookEntryForWidgetInScope
+    ),
     vscode.languages.registerCodeActionsProvider(
       DART_MODE,
       new WidgetbookEntriesCodeActions()
@@ -34,6 +37,30 @@ async function generateWidgetbookEntryForWidgetInScope(): Promise<void> {
   const clazz = parseTextToClass(fileContentFromCurrentLine);
 
   await writeWidgetbookEntry(clazz);
+}
+async function generateWidgetbookEntriesForDirectory(
+  uri: vscode.Uri
+): Promise<void> {
+  const directoryPath = uri.fsPath;
+
+  const files = await vscode.workspace.fs.readDirectory(
+    vscode.Uri.file(directoryPath)
+  );
+
+  for (const [fileName, fileType] of files) {
+    if (fileType === vscode.FileType.File) {
+      const filePath = path.join(directoryPath, fileName);
+
+      const fileContent = await vscode.workspace.fs.readFile(
+        vscode.Uri.file(filePath)
+      );
+      const fileContentString = new TextDecoder().decode(fileContent);
+
+      const clazz = parseTextToClass(fileContentString);
+
+      await writeWidgetbookEntry(clazz);
+    }
+  }
 }
 
 export { activate };
