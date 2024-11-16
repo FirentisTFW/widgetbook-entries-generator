390b65fa1963c4ff55b47fc6d94f6626daedf8cc
Paweł Jakubowski <jkb.pawel@gmail.com>
2024-11-16 11:38:47 +0100

    Handle errors - display a dialog with a link to create an issue

--- src/extension.ts
+++ src/extension.ts
@@ -6,22 +6,46 @@ import {
 } from "./commands";
 
 const DART_MODE = { language: "dart", scheme: "file" };
-
 function activate(context: vscode.ExtensionContext) {
   context.subscriptions.push(
     vscode.commands.registerCommand(
       "widgetbook-generator.generate.directory",
-      generateWidgetbookEntriesForDirectory
+      (uri: vscode.Uri) =>
+        runCatching(() => generateWidgetbookEntriesForDirectory(uri))
     ),
     vscode.commands.registerCommand(
       "widgetbook-generator.generate.widget",
-      generateWidgetbookEntryForWidgetInScope
+      () => runCatching(() => generateWidgetbookEntryForWidgetInScope())
     ),
     vscode.languages.registerCodeActionsProvider(
       DART_MODE,
       new WidgetbookEntriesCodeActions()
     )
   );
+}
+
+async function runCatching(callback: () => Promise<void>) {
+  const createIssueText = "Create issue";
+
+  try {
+    await callback();
+  } catch (error) {
+    vscode.window
+      .showErrorMessage(
+        `Failed to execute action. Error: ${error}. ` +
+          `Please create an issue in extension's repository with minimal reproducible example.`,
+        createIssueText
+      )
+      .then((selected) => {
+        if (selected === createIssueText) {
+          vscode.env.openExternal(
+            vscode.Uri.parse(
+              "https://github.com/leancodepl/widgetbook-entries-generator/issues"
+            )
+          );
+        }
+      });
+  }
 }
 
 export { activate };
