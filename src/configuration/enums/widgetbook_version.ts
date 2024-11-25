35b1e3daa891d9969a03f3ed4b9f2c268171030b
Paweł Jakubowski <jkb.pawel@gmail.com>
2024-11-25 21:44:41 +0100

    Support "Latest" version

--- src/configuration/enums/widgetbook_version.ts
+++ src/configuration/enums/widgetbook_version.ts
@@ -11,6 +11,7 @@ function parseWidgetbookVersion(version: string): WidgetbookVersion {
     case "3.2.0":
       return WidgetbookVersion.v3_2_0;
     case "3.4.0":
+    case "Latest":
       return WidgetbookVersion.v3_4_0;
   }
 
